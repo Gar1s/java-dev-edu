11f28dd7537f34058cf4b30010f6fbdf8ba47271
eddie <partyka.eduard@gmail.com>
2025-07-11 17:29:11 +0300

    [master] refactored

--- components/app/dashboard-sidebar.vue
+++ components/app/dashboard-sidebar.vue
@@ -21,7 +21,6 @@ const items = ref<NavigationMenuItem[][]>([
         },
         {
           label: 'Collections',
-          to: '/java/collections',
           icon: 'i-lucide-library',
         },
         {
@@ -55,62 +54,52 @@ const items = ref<NavigationMenuItem[][]>([
           label: 'JDBC',
           icon: 'i-lucide-file-text',
           description: 'Define shortcuts for your application.',
-          to: '/dbms/jdbc'
         },
         {
           label: 'useOverlay',
           icon: 'i-lucide-file-text',
           description: 'Display a modal/slideover within your application.',
-          to: '/composables/use-overlay'
         },
         {
           label: 'useToast',
           icon: 'i-lucide-file-text',
           description: 'Display a toast within your application.',
-          to: '/composables/use-toast'
         }
       ]
     },
     {
       label: 'Components',
       icon: 'i-lucide-box',
-      to: '/components',
       children: [
         {
           label: 'Link',
           icon: 'i-lucide-file-text',
           description: 'Use NuxtLink with superpowers.',
-          to: '/components/link'
         },
         {
           label: 'Modal',
           icon: 'i-lucide-file-text',
           description: 'Display a modal within your application.',
-          to: '/components/modal'
         },
         {
           label: 'NavigationMenu',
           icon: 'i-lucide-file-text',
           description: 'Display a list of links.',
-          to: '/components/navigation-menu'
         },
         {
           label: 'Pagination',
           icon: 'i-lucide-file-text',
           description: 'Display a list of pages.',
-          to: '/components/pagination'
         },
         {
           label: 'Popover',
           icon: 'i-lucide-file-text',
           description: 'Display a non-modal dialog that floats around a trigger element.',
-          to: '/components/popover'
         },
         {
           label: 'Progress',
           icon: 'i-lucide-file-text',
           description: 'Show a horizontal bar to indicate task progression.',
-          to: '/components/progress'
         }
       ]
     }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,9 @@ export default defineNuxtConfig({
   modules: ['@nuxt/ui', '@nuxt/content'],
   css: ["~/assets/css/main.css"],
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  // app: {
+  //   buildAssetsDir: 'assets', // for static deployment uncomment this line
+  // }
+  ssr: false,
 })
